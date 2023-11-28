85c1a5ba6e666f24baff0e90a29163c0fda7bb00
= <=>
2023-11-28 17:35:57 +0700

    Starter files

---
+++ src/Sword.java
@@ -0,0 +1,30 @@
+public class Sword {
+
+    // Sword attributes
+    private double baseDamage;
+    private double damage;
+    private int level;
+
+    public Sword() {
+        this.baseDamage = 100;
+        this.level = 1;
+        updateDamage();
+    }
+
+    public void levelUp() {
+        level++;
+        updateDamage();
+    }
+
+    private void updateDamage() {
+        damage = baseDamage * (1 + 0.1 * level);
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+}
